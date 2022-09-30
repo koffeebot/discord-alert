c4f342a4d82d3af86c1dc7e4d7a71a19711470db
koffeebot <daniel@robolabs.biz>
2022-09-30 15:11:59 +0800

    update settings

--- src/util/settings.ts
+++ src/util/settings.ts
@@ -1,12 +1,10 @@
 
 import * as process from 'process'
-
-
-
+import '../config'
 export class Settings {
 
+
     public static get (setting: string): string {
-        console.log(process.env['TEST'])
         if (!process.env[setting]) {
             throw new Error(`ENV: ${setting} not configured`)
         }
