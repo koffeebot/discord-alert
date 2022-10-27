6f189da35ff2f29e7b8ea73fc949d8479f18801f
koffeebot <daniel@robolabs.biz>
2022-10-27 12:11:37 +0800

    fix alerts on vautl details

--- src/monitors/api-monitor.ts
+++ src/monitors/api-monitor.ts
@@ -1,25 +1,23 @@
 import { DiscordAlertsChannel } from '../apis/discordalerts'
 import Axios from 'axios'
-import e from 'cors'
-import { isTemplateExpression } from 'typescript'
 
 export class ApiMonitor {
     private url: string
     private alerts: DiscordAlertsChannel
     private data?: any[]
-    private message?: string
+    private message?: string = ''
     // private online?: boolean
     // private alerted?: boolean
 
     constructor (alerts: DiscordAlertsChannel, url: string) {
         this.url = url
         this.alerts = alerts
-
+        
     }
 
     async init () {
         //interval of 10 mins
-        setInterval(this.apiCheck.bind(this), 60 * 10 * 1000)
+        setInterval(this.apiCheck.bind(this), 60 * 1000 * 10)
         setTimeout(this.apiCheck.bind(this), 2000)
     }
 
@@ -55,6 +53,8 @@ export class ApiMonitor {
             chain: item.chain,
             vaultId: item.vaultId,
             tvlUsd: item.tvlUsd,
+            apy1d: item.apy1d,
+            apy2d: item.apy2d,
             apy3d: item.apy3d,
             apy7d: item.apy7d,
             apy14d: item.apy14d,
@@ -62,16 +62,17 @@ export class ApiMonitor {
         }))
 
         for (const vault of vaults) {
-            // console.log(vault)
-            // console.log(vault.tvlUsd == 0)
-            if (!vault.tvlUsd || vault.tvlUsd === 0) { this.message += `Tvl is empty/no Value on ` + `${vault.chain} ` + `${vault.vaultId}` }
-            if (!vault.apy3d || vault.apy3d === 0) { this.message += 'apy3d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId}` }
-            if (!vault.apy7d || vault.apy7d === 0) { this.message += 'apy7d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId}` }
-            if (!vault.apy14d || vault.apy14d === 0) { this.message += 'apy14d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId}` }
-            if (!vault.apy28d || vault.apy28d === 0) { this.message += 'apy28d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId}` }
-            if (this.message === null) { this.sendAlert(this.message) }
 
+            if (!vault.tvlUsd || vault.tvlUsd === 0) { this.message += `Tvl is empty/no Value on ` + `${vault.chain} ` + `${vault.vaultId} \n` }
+            if (!vault.apy1d || vault.apy1d === 0) { this.message += 'apy3d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId} \n` }
+            if (!vault.apy2d || vault.apy2d === 0) { this.message += 'apy3d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId} \n` }
+            if (!vault.apy3d || vault.apy3d === 0) { this.message += 'apy3d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId} \n` }
+            if (!vault.apy7d || vault.apy7d === 0) { this.message += 'apy7d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId} \n` }
+            if (!vault.apy14d || vault.apy14d === 0) { this.message += 'apy14d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId} \n` }
+            if (!vault.apy28d || vault.apy28d === 0) { this.message += 'apy28d is empty/no Value ' + `${vault.chain} ` + `${vault.vaultId} \n` }
         }
+
+        if (this.message !== '') { this.sendAlert(this.message) }
 
     }
 
